72490ba0f1b8a6e276e14adfff36ad7765ce2fa7
inigo10rodri <inigo10rodri@gmail.com>
2018-01-31 10:05:34 +0100

    Contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,21 +1,59 @@
 <template>
-    <div>
-        <form>
-            <label>Usuario:</label><br>
-            <input type="text" name="usuario" placeholder="Username"><br>
-            <label>Contraseña:</label><br>
-            <input type="password" name="contraseña" placeholder="Password"><br>
-            <label>Mensaje:</label><br>
-            <textarea name="textarea"></textarea><br>
-            <button>Enviar</button>
-        </form>
+    <div id="contact-form">
+        <div class="contact-content">
+            <h1>Contacto</h1><br>
+            <form>
+                <label>Nombre:</label><br>
+                <input type="text" name="nombre" placeholder="Name"><br>
+                <label>Email:</label><br>
+                <input type="email" name="email" placeholder="Email"><br>
+                <label>Mensaje:</label><br>
+                <textarea name="textarea"></textarea><br>
+                <input type="button" class="contact contact-button" value="Enviar">
+            </form>
+        </div>
     </div>
 
 </template>
 
 <script>
-    export default{
+    export default {
         name: 'Contact',
 
+        data() {
+            return {}
+        },
+        methods: {
+
+            // ...mapGetters({}), if needed ,put it in computed
+
+            ...mapActions(
+                {
+                    bindName: 'updateNameAction',
+                    bindEmail: 'updateEmailAction',
+                    contact: 'contactAction'
+                }),
+
+            updateEmail: function (event) {
+                console.log(event.target.value);
+                this.bindEmail(event.target.value);
+
+            },
+
+            updateName: function (event) {
+                console.log(event.target.value);
+                this.bindPassword(event.target.value);
+            },
+
+            loginOrFail: function () {
+                this.login();
+
+                this.$router.push({
+                    name: 'profile'
+                });
+            },
+
+
+        }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import '@/assets/profile/profile.css'
 import '@/assets/profile/profile.js'
 import '@/assets/leaflet/plugins/location/L.Control.Locate.min.css'
 import '@/assets/leaflet/plugins/location/L.Control.Locate.min.js'
+import '@/assets/contact/contact.css'
 
 
 import Vue from 'vue'
